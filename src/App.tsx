2acbdb82499df297406158bfbca9b887c9a44b3e
theroamer <st880221@gmail.com>
2025-06-03 03:01:46 +0800

    ✨ Feat: Add event page with route params

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import HomePage from './pages/HomePage'
 import ContactPage from './pages/ContactPage'
 import AboutPage from './pages/AboutPage'
 import NotFoundPage from './pages/NotFoundPage'
+import EventPage from './pages/EventPage'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/contact" element={<ContactPage />} />
+          <Route path="/events/:eventId" element={<EventPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </BrowserRouter>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom"
+
 const HomePage = () => {
   return (
     <div>
@@ -6,6 +8,22 @@ const HomePage = () => {
         subtitle="探索我們的藝文共享空間"
         description="在這裡，您可以與其他藝術愛好者分享您的創作，並欣賞他們的作品。無論是音樂、繪畫還是其他形式的藝術，我們都歡迎您的加入！"
       />
+
+      <section className="events">
+        <h2>即將舉辦的活動</h2>
+        <p>敬請期待我們即將舉辦的藝文活動，讓我們一起探索藝術的無限可能！</p>
+        <ul>
+          <li>
+            <NavLink to="/events/1">心與星的距離 - 李芳個人小型畫展</NavLink>
+          </li>
+          <li>
+            <NavLink to="/events/2">科技與藝術的交融 - 藝術家論壇</NavLink>
+          </li>
+          <li>
+            <NavLink to="/events/3">音樂與自然 - 戶外音樂會</NavLink>
+          </li>
+        </ul>
+      </section>
     </div>
   )
 }
